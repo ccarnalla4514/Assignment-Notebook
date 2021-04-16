950143386ba6c531d2213f8a7eee9e1d455f5bd8
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-04-16 15:13:21 -0500

    map

--- Assignment Notebook/ViewController.swift
+++ Assignment Notebook/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    let assignments = ["Geometry packet", "Computer Programming Final Unit App", "Biology Classkick"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return assignments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let mycell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        mycell.textLabel!.text = assignments[indexPath.row]
+   return mycell
+    }
+    
 
 }
 
